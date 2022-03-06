61360cab918a793b6304cfb28902c4d3ea059e88
snehamahapatra <mahapat0@purdue.edu>
2022-03-06 13:27:33 -0500

    ready for simple demo. Essentially we will need to add backend

--- my-app/App.js
+++ my-app/App.js
@@ -1,101 +1,180 @@
 import * as React from 'react';
-import {SectionList, TextInput, StyleSheet, Text, View, Dimensions, Button, SafeAreaView, Alert} from 'react-native';
+import {SectionList, TextInput, StyleSheet, Text, View, Dimensions,Switch, Button, SafeAreaView, Alert, Platform, 
+TouchableWithoutFeedback, Keyboard
+} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MapView from 'react-native-maps';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { Marker } from "react-native-maps";
 
+class MyCalendar extends React.Component {
+  months = ["January", "February", "March", "April", 
+ "May", "June", "July", "August", "September", "October", 
+ "November", "December"];
+
+ weekDays = [
+     "Sun","Mon","Tue","Wed","Thu","Fri","Sat"
+ ]; 
+ nDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+ state = {
+  activeDate: new Date()
+} 
+
+setMonth = month => {  
+  let monthNo = this.months.indexOf(month);// get month number  
+  let dateObject = Object.assign({}, this.state.dateObject);  
+  dateObject = moment(dateObject).set("month", monthNo); // change month value  
+  this.setState({  
+  dateObject: dateObject // add to state  
+  });  
+  };   
+
+  
+  render() {
+    var year = this.state.activeDate.getFullYear();
+    var month = this.state.activeDate.getMonth();
+    var firstDay = new Date(year, month, 1).getDay(); 
+    var maxDays = this.nDays[month];
+    if (month == 1) { // February
+      if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        maxDays += 1;
+      }
+    } 
+    changeMonth = (n) => {
+      this.setState(() => {
+        this.state.activeDate.setMonth(
+          this.state.activeDate.getMonth() + n
+        )
+        return this.state;
+      });
+  } 
+    props.data.map(data => {  
+      months.push(  
+      <td  
+      key={data}  
+      className="calendar-month"  
+      onClick={e => {  
+      this.setMonth(data);  
+      }}  
+      >  
+      <span>{data}</span>  
+      </td>  
+      );  
+      });   
+    var rows = [];
+    rows = matrix.map((row, rowIndex) => {
+      var rowItems = row.map((item, colIndex) => {
+        return (
+          <Text
+            style={{
+              flex: 1,
+              height: 18,
+              textAlign: 'center',
+              // Highlight header
+              backgroundColor: rowIndex == 0 ? '#ddd' : '#fff',
+              // Highlight Sundays
+              color: colIndex == 0 ? '#a00' : '#000',
+              // Highlight current date
+              fontWeight: item == this.state.activeDate.getDate() 
+                                  ? 'bold': ''
+            }}
+            onPress={() => this._onPress(item)}>
+            {item != -1 ? item : ''}
+          </Text>
+        );
+      });
+      return (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            padding: 15,
+            justifyContent: 'space-around',
+            alignItems: 'center',
+          }}>
+          {rowItems}
+        </View>
+      );
+    }); 
+
+    return (
+      <View>
+      <Text style={{
+        fontWeight: 'bold',
+        fontSize: 18,
+        textAlign: 'center'
+      }}>
+        {this.months[this.state.activeDate.getMonth()]}  
+        {this.state.activeDate.getFullYear()}
+      </Text> 
+      <Button title="Previous"
+         onPress={() => this.changeMonth(-1)}/>
+       <Button title="Next"
+         onPress={() => this.changeMonth(+1)}/> 
+
+
+      </View>
+    );
+  }
+} 
+
+const DismissKeyboard = ({children}) => (
+  <TouchableWithoutFeedback onPress = {() => Keyboard.dismiss()}>
+    [children]
+  </TouchableWithoutFeedback>
+);
 const Separator = () => (
   <View style={styles.separator} />
 );
 
 const newTaskData = [{
-  title: "New Tasks",
+  title: "Avaliable",
   data: [
     {
       id: "1",
-      task: "Buy groceries"
+      task: "Agent A"
     },
     {
       id: "2",
-      task: "Feed Cat"
+      task: "Agent B"
     },
     {
       id: "3",
-      task: "Sleep for 3 hours"
+      task: "Agent C"
     },
     {
       id: "4",
-      task: "Water Plants"
+      task: "Agent D"
     },
     {
       id: "5",
-      task: "Drink Water"
+      task: "Agent E"
     },
-  ]
-}];
-const completedTaskData = [{
-  title: "Completed Tasks",
-  data: [
     {
       id: "6",
-      task: "Make a section list tutorial"
+      task: "Agent F"
     },
     {
       id: "7",
-      task: "Share this tutorial"
+      task: "Agent G"
     },
     {
       id: "8",
-      task: "Ask doubt in the Comments"
+      task: "Agent H"
     },
     {
       id: "9",
-      task: "Subscribe to logrocket"
+      task: "Agent H"
     },
     {
       id: "10",
-      task: "Read next Article"
-    },
-    {
-      id: "11",
-      task: "Read next Article 2"
-    },
-    {
-      id: "12",
-      task: "Read next Article 3"
-    },
-    {
-      id: "13",
-      task: "Read next Article 4"
-    },
-    {
-      id: "14",
-      task: "Read next Article 5"
-    },
-    {
-      id: "15",
-      task: "Read next Article 6"
-    },
-    {
-      id: "16",
-      task: "Read next Article 7"
-    },
-    {
-      id: "17",
-      task: "Read next Article 8"
-    },
-    {
-      id: "18",
-      task: "Read next Article 9"
-    },
-    {
-      id: "19",
-      task: "Read next Article 10"
+      task: "Agent I"
     },
   ]
 }];
+
 
 const Stack = createNativeStackNavigator();
 const num  = 0;
@@ -103,8 +182,35 @@ const num  = 0;
 function CaseNumber({ navigation }) {
   const [text, onChangeText] = React.useState("Enter Case Number");
   const [number, onChangeNumber] = React.useState(null);
+  const [date_start, startDate] = React.useState(0); 
+  const [date_end, endDate] = React.useState(0); 
+  const vacation = {key: 'vacation', color: 'red', selectedDotColor: 'blue'};
+  const massage = {key: 'massage', color: 'blue', selectedDotColor: 'blue'};
+  const workout = {key: 'workout', color: 'green'};
+  const [showCalendar, stateCalendar] = React.useState(false);
+
   return (
+    
     <SafeAreaView style={styles.container}>
+       <View style={{flexDirection:"row"}}>
+      <View style={{flex:1}}>
+
+          <Text>Start Date:</Text>
+
+          <TextInput placeholder="1/03/2022" placeholderTextColor="gray" style={styles.input} />
+      </View>
+      <View style={{flex:1}}>
+      <Text>End Date:</Text>
+          <TextInput placeholder="01/04/2022" placeholderTextColor="gray" style={styles.input} />
+      </View>
+     </View>
+
+     <Button
+        title="Submit"
+        color="#f194ff"
+        onPress={() => stateCalendar(!showCalendar)}
+      />
+
     <Text>
       Case Number:
     </Text>
@@ -113,6 +219,7 @@ function CaseNumber({ navigation }) {
         onChangeText={onChangeNumber}
         value={number}
         num={number}
+        placeholderTextColor="gray"
         placeholder="Enter Case Number"
         keyboardType="numeric"
       />
@@ -125,87 +232,228 @@ function CaseNumber({ navigation }) {
         onPress={() => navigation.navigate('Canvas')}
       />
     </View>
+    
+    
+        {showCalendar ? (<Calendar
+              markingType={'multi-dot'}
+
+          markedDates={{
+        '2022-03-16': {selected: true, marked: true, selectedColor: 'blue'},
+        '2022-03-17': {dots: [vacation, massage, workout],marked: true},
+        '2022-03-18': {dots: [vacation, massage, workout],marked: true, dotColor: 'red', activeOpacity: 0},
+        '2022-03-25': {dots: [workout], selected: true, selectedColor: 'red'},
+
+        '2022-03-19': {dots: [vacation, workout],disabled: true, disableTouchEvent: true}
+      }}
+              // Initially visible month. Default = Date()
+              current={'2022-03-05'}
+              // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+              minDate={'2032-05-10'}
+              // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+              maxDate={'2032-05-30'}
+              // Handler which gets executed on day press. Default = undefined
+              onDayPress={day => {
+                console.log('selected day', day);
+              }}
+              // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+              monthFormat={'yyyy MM'}
+              // Handler which gets executed when visible month changes in calendar. Default = undefined
+              onMonthChange={month => {
+                console.log('month changed', month);
+              }}
+              // Hide month navigation arrows. Default = false
+              hideArrows={false}
+              // Do not show days of other months in month page. Default = false
+              hideExtraDays={true}
+              // If hideArrows=false and hideExtraDays=false do not swich month when tapping on greyed out
+              // day from another month that is visible in calendar page. Default = false
+              disableMonthChange={false}
+              // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+              firstDay={1}
+            />
+        ) : null}
+
     <Separator />
+  
+   
+
   </SafeAreaView>
   );
 }
 
 
+
 function InvestigatorCanvas({ navigation }) {
   const [text, onChangeText] = React.useState("Enter Case Number");
   const [number, onChangeNumber] = React.useState(null);
- 
+  const [showList, state] = React.useState(false);
+
+
   return (
     <View>
-    <Calendar
-          // Initially visible month. Default = Date()
-          current={'2022-03-05'}
-          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-          minDate={'2032-05-10'}
-          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-          maxDate={'2032-05-30'}
-          // Handler which gets executed on day press. Default = undefined
-          onDayPress={day => {
-            console.log('selected day', day);
-          }}
-          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-          monthFormat={'yyyy MM'}
-          // Handler which gets executed when visible month changes in calendar. Default = undefined
-          onMonthChange={month => {
-            console.log('month changed', month);
-          }}
-          // Hide month navigation arrows. Default = false
-          hideArrows={false}
-          // Do not show days of other months in month page. Default = false
-          hideExtraDays={false}
-          // If hideArrows=false and hideExtraDays=false do not swich month when tapping on greyed out
-          // day from another month that is visible in calendar page. Default = false
-          disableMonthChange={false}
-          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-          firstDay={1}
-        />
-    <Button
-      title="Available Investigators"
-      color="#669fa8"
-    />
+     
     <View style={{flexDirection:"row"}}>
       <View style={{flex:1}}>
-          <TextInput placeholder="Latitude" style={styles.input} />
+          <TextInput placeholder="Latitude" placeholderTextColor="gray" style={styles.input} />
       </View>
       <View style={{flex:1}}>
-          <TextInput placeholder="Longitude" style={styles.input} />
+          <TextInput placeholder="Longitude"  placeholderTextColor="gray" style={styles.input} />
       </View>
-  </View>
-  <Button
-      title="Submit"
-      color="#669fa8"
-    />
-      <SectionList
-        sections={[...newTaskData, ...completedTaskData]}
-        renderItem={({item})=>(
-            <Text style={styles.taskItem}>{item.task}</Text>
-        )}
-        renderSectionHeader={({section})=>(
-          <Text style={styles.taskTitle}>{section.title}</Text>
-        )}
-        keyExtractor={item=>item.id}
-        stickySectionHeadersEnabled
+     </View>
+     <View style={{flexDirection:"column"}}>
+     <View>
+
+     <Button
+        title="Available Investigators"
+        color="#669fa8"
+        onPress={() => state(!showList)}
       />
   </View>
+     
+     <Button
+     title="Check Live Location"
+     color="#669fa8"
+     onPress={() => navigation.navigate('Map')}
+   />
+     {showList ? (
+              <SectionList
+          sections={[...newTaskData]}
+          renderItem={({item})=>(
+              <Text style={styles.taskItem}>{item.task}</Text>
+          )}
+          renderSectionHeader={({section})=>(
+            <Text style={styles.taskTitle}>{section.title}</Text>
+          )}
+          keyExtractor={item=>item.id}
+          stickySectionHeadersEnabled
+        />
+        ) : null}
+
+       
+   
+     </View>
+
+
+
+<View>
+     
+    </View>
+  </View>
+  );
+}
+function CustomMarker({ name }) {
+  return (
+    <View style={styles.marker}>
+      <Text style={styles.color}>{name}</Text>
+    </View>
   );
 }
 
 function MapScreen() {
+  const coordinate1 = {
+    latitude: 37.7885,
+    longitude: -122.435,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+  const coordinate2 = {
+    latitude: 37.75,
+    longitude: -122.433,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+  const coordinate3 = {
+    latitude: 37.78,
+    longitude: -122.431,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+  const coordinate4 = {
+    latitude: 37.79,
+    longitude: -122.429,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+  const coordinate5 = {
+    latitude: 37.7877,
+    longitude: -122.427,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+  const coordinate6 = {
+    latitude: 37.7875,
+    longitude: -122.425,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+  const coordinate7 = {
+    latitude: 37.7873,
+    longitude: -122.435,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+  const coordinate8 = {
+    latitude: 37.7871,
+    longitude: -122.435,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+  const coordinate9 = {
+    latitude: 37.7869,
+    longitude: -122.435,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+  const coordinate10 = {
+    latitude: 37.7867,
+    longitude: -122.435,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+
+
   return (
       <View style={styles.containerMap}>
-        <MapView style={styles.map} />
-      </View>
+    {/*Render our MapView*/}
+      <MapView
+        style={styles.map}
+        //specify our coordinates.
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        >
+         <Marker coordinate={coordinate1}>
+          <CustomMarker name="Agent A"/>
+        </Marker>
+         <Marker coordinate={coordinate2} >
+          <CustomMarker name="Agent B"/>
+        </Marker>
+         <Marker coordinate={coordinate3} >
+          <CustomMarker name="Agent C"/>
+        </Marker>
+         <Marker coordinate={coordinate4} >
+          <CustomMarker name="Agent D"/>
+        </Marker>
+         <Marker coordinate={coordinate5} />
+         <Marker coordinate={coordinate6} />
+         <Marker coordinate={coordinate7} />
+         <Marker coordinate={coordinate8} />
+         <Marker coordinate={coordinate9} />
+         <Marker coordinate={coordinate10} />
+
+      </MapView>
+    </View>
   );
 }
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -251,6 +499,7 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    textShadowColor: '#000'
   },
   container_scroll: {
     flex: 1,
@@ -284,6 +533,32 @@ const styles = StyleSheet.create({
     margin: 10,
     marginBottom: 0,
     borderRadius: 10
-  }
+  },
+  root: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  inlineSwitchContainer: {
+    marginTop: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  inlineSwitchText: {
+    fontSize: 18,
+    marginRight: 8,
+  }, 
+  
+  marker: {
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    backgroundColor: "#007bff",
+    borderColor: "#eee",
+    borderRadius: 5,
+    elevation: 10,
+  },
+  text: {
+  color: "#fff",   
+  },
 
 });
